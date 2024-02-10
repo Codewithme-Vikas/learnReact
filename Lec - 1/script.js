8dcea3c46922c81f72c2337b184e3ca6436f67fd
Vikash Gurjar <vikashnagar2025@gmail.com>
2024-02-10 15:31:33 +0530

    Lecture 1 Learning about how to use React by just link and why React is a library

---
+++ Lec - 1/script.js
@@ -0,0 +1,9 @@
+// const div = document.getElementById("root");
+
+// let pTag = document.createElement("p");
+
+// pTag.innerHTML = "by the javascript";
+
+// div.appendChild(pTag);
+
+// console.log(pTag, "see in the consle");
